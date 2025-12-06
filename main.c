d8c4b2ffbc439e3e84cfbafb94f0ce5ce213b2b3
Khal <muhdkhalishreeza@gmail.com>
2025-12-06 16:58:28 +0800

    Member 5 added int main function Added int main function
    Signed-off-by: Khal <muhdkhalishreeza@gmail.com>

--- main.c
+++ main.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+
+//Member 5
+int main(int argc, char*argv[])
+{
+    int a = 13;
+    int b = 10;
+    printf("Simple Calculator\n");
+    print("a + b = %d\n", add_funct(a,b));
+    print("a + b = %d\n", sub_funct(a,b));
+    print("a + b = %d\n", mul_funct(a,b));
+    print("a + b = %d\n", div_funct(a,b));
+
+    return 0;
+}
